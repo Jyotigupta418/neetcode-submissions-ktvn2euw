b1119461a98cf32036f5b8263b9033d6c16bf2c6
Jyotigupta418 <143078953+Jyotigupta418@users.noreply.github.com>
2026-04-08 22:12:17 +0545

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let left = 0;
+        let right = s.length-1
+        
+        while(left<right){
+            while(left<right && !this.isAlphaNo(s[left])) left++;
+            while(left<right && !this.isAlphaNo(s[right])) right--;
+
+            if(s[left].toLowerCase() !== s[right].toLowerCase()) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    isAlphaNo(s){
+       return /^[a-z0-9]$/i.test(s)
+    }
+}
